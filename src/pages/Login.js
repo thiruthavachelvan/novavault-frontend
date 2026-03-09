987241265385d6f9bc48d8b3f9cd5b4529091ac0
Thiru <thirukumar3210@gmail.com>
2026-03-10 03:09:49 +0530

    Remove unused useNavigate import in Login.js (fix ESLint no-unused-vars)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Login() {
     const [email, setEmail] = useState("");
